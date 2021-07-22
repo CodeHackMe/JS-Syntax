0f6058c38c63a02dbc64faffdefce77a76c5f59a
CodeHackMe <87801805+CodeHackMe@users.noreply.github.com>
2021-07-22 15:38:04 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var firstname="Caleb"; 
+console.log (firstname);
+var lastname="Leyson";
+console.log (lastname);
+var fullname=firstname.concat(lastname);
+console.log (fullname);
+var mylength=fullname.length;
+console.log(mylength);
+var sister=fullname.replace("Caleb","Zoe");
+console.log(sister);
+var upper=sister.toUpperCase();
+console.log(upper);
+var squareroot=Math.sqrt(144);
+console.log(squareroot);
+var maxnum=Math.max(167,858);
+console.log(maxnum);
+var minnum=Math.min(46,80)
+console.log(minnum);
